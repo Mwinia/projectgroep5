09360c1ae687f2e1ddfe621755ba459e8905bad7
Melvin <mail@melvinwinia.nl>
2018-05-17 14:52:01 +0200

    prachtig

--- functies.php
+++ functies.php
@@ -56,6 +56,17 @@ function AanvragenBrochure($aanvraag_gegevens) {
 
         $mail = new PHPMailer\PHPMailer\PHPMailer();
         try {
+			//Server settings
+			$mail->SMTPDebug = 2;                                 // Enable verbose debug output
+			$mail->isSMTP();                                      // Set mailer to use SMTP
+			$mail->Host = 'smtp1.example.com;smtp2.example.com';  // Specify main and backup SMTP servers
+			$mail->SMTPAuth = true;                               // Enable SMTP authentication
+			$mail->Username = 'user@example.com';                 // SMTP username
+			$mail->Password = 'secret';                           // SMTP password
+			$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+			$mail->Port = 587;                                    // TCP port to connect to
+	
+			//Recipients
             $mail->setFrom('brochure@windesheim.nl', 'Windesheim');
             $mail->addAddress($emailadres);
 
